4cf331056bf2e42ccee9265079f3e34b986d9cf6
ItzTas <talessabini2@gmail.com>
2024-08-04 00:00:40 -0300

    primeiro exercicio

--- mobile-first/tp1/src/App.jsx
+++ mobile-first/tp1/src/App.jsx
@@ -1,5 +1,14 @@
 import './App.css';
+import React from 'react';
+import Ex1 from './atv1/ex1/ex1';
 
 export default function App() {
-    return <main>React ⚛️ + Vite ⚡ + Replit</main>;
+  return (
+    <main>
+      <section>
+        <h2>Atividade 1</h2>
+        <Ex1 />
+      </section>
+    </main>
+  );
 }
---
+++ mobile-first/tp1/src/atv1/ex1/ex1.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ex1.css';
+
+function Ex1() {
+  return (
+    <div className='container'>
+      <div className='box-1' />
+      <div className='box-2' />
+      <div className='box-3' />
+    </div>
+  );
+}
+
+export default Ex1;
